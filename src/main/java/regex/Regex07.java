c18da1c0078905cc4c5d3d820bf4d877848dde43
段平刚 <260477@gree.com.cn>
2021-08-10 17:10:32 +0800

    refactor: 修改中文字符正则
    修改中文字符正则

--- src/main/java/regex/Regex07.java
+++ src/main/java/regex/Regex07.java
@@ -2,14 +2,14 @@ package regex;
 
 public class Regex07 {
     public static void main(String[] args) {
-        String[] strings =new String[]{"56-2D-C9-75","F4911E698141","EtwfTFEFFr{"};
+        String[] strings =new String[]{"3","好好说话","上看看{"};
         for(String s: strings) {
             System.out.println(regex07(s));
         }
     }
 
     public static boolean regex07(String str){
-        String reg ="^[-a-zA-Z0-9]{1,12}$";
+        String reg ="^[\\u4E00-\\u9FA5]+$";
         return str.matches(reg);
     }
 }
